42e95439f87a340055299df27249c1c8ce7f75e6
RobertoCastelli <roberto.castelli.teal@gmail.com>
2026-07-10 00:07:21 +0200

    client: link logo to homepage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import logo from "../assets/logo.png";
 
 function Header() {
   return (
     <header className="header-container">
-      <img className="header-logo" src={logo} alt="museum event placeholder" />
-
+      <Link to="/">
+        <img
+          className="header-logo"
+          src={logo}
+          alt="museum event placeholder"
+        />
+      </Link>
       <Navbar />
     </header>
   );
